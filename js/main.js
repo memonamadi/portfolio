6157ba707e99ee2f2d75273de7f7fad2315694eb
Memona Madi <madi.memona@gmail.com>
2015-03-21 12:57:20 +0000

    First commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+(function() {
+
+  // function init 
+  function init() {
+    smoothScroll();
+    animateArrow();
+  }
+
+  function smoothScroll() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  }
+
+  function animateArrow() {
+    setInterval(function(){
+      $('.arrow').animate({bottom:'20px'}, 500, function(){
+          $(this).animate({bottom:'30px'}, 500);
+      });
+
+    }, 1000);
+  }
+
+  init();
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
